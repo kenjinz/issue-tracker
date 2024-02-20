6aefbb3360290d9d726ee28cfbe9d3abde479219
kenjinz <Ken.Cry.K.7852396541@gmail.com>
2024-02-20 23:44:07 +0700

    Extract the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,31 @@
 'use client';
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import 'easymde/dist/easymde.min.css';
 import dynamic from 'next/dynamic';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
+import SimpleEditor from 'react-simplemde-editor';
+import ErrorMessage from '@/app/components/ErrorMessage';
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
-  const router = useRouter();
-  const SimpleMdeEditor = dynamic(() => import('react-simplemde-editor'), {
-    ssr: false,
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
+  const router = useRouter();
+  // const SimpleMdeEditor = dynamic(() => import('react-simplemde-editor'), {
+  //   ssr: false,
+  // });
   const [error, setError] = useState('');
   return (
     <div className="max-w-xl">
@@ -39,13 +48,16 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register('title')} />
         </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
           render={({ field }) => (
-            <SimpleMdeEditor placeholder="description" {...field} />
+            <SimpleEditor placeholder="description" {...field} />
           )}
         />
+
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button>Submit New Issue</Button>
       </form>
