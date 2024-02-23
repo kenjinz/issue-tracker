7a91a58ed28181b8873f7a89b9ad7146fad865ac
kenjinz <Ken.Cry.K.7852396541@gmail.com>
2024-02-23 23:07:13 +0700

    Delete an issue

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -24,7 +24,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box>
         <Flex direction="column" gap="4">
           <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton />
+          <DeleteIssueButton issueId={issue.id} />
         </Flex>
       </Box>
     </Grid>
