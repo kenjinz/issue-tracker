3ba80eb566aa3c7c18a61de944c6abbc364dc2b9
kenjinz <Ken.Cry.K.7852396541@gmail.com>
2024-03-11 23:24:50 +0700

    configure google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,12 @@
 import NextAuth from 'next-auth';
+import GOOGLE_FONT_PROVIDER from 'next-auth/providers/google';
 const handler = NextAuth({
-  providers: [],
+  providers: [
+    GOOGLE_FONT_PROVIDER({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+  ],
 });
 
 export { handler as GET, handler as POST };
