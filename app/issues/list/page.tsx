23066b659ac96ec2a01f88b8c3bdc5585f538d67
kenjinz <Ken.Cry.K.7852396541@gmail.com>
2024-03-17 17:09:58 +0700

    Sort issue

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -23,10 +23,19 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = Object.values(Status).includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? {
+        [searchParams.orderBy]: 'asc',
+      }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
