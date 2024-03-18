6105c7f022eff8e63cdbb392be58f57e482c6212
kenjinz <Ken.Cry.K.7852396541@gmail.com>
2024-03-18 23:06:05 +0700

    Build layout of pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import Pagination from './components/Pagination';
 
 export default function Home() {
-  return <div>ABC</div>;
+  return (
+    <div>
+      <Pagination itemCount={100} pageSize={10} currentPage={1} />
+    </div>
+  );
 }
