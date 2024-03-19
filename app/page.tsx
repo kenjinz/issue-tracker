7c4b3d13006cb7ecbde19ebb03032af15e40cd0f
kenjinz <Ken.Cry.K.7852396541@gmail.com>
2024-03-19 23:16:22 +0700

    Layout the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image';
-import Pagination from './components/Pagination';
-import LatestIssues from './LatestIssues';
-import IssueSummary from './IssueSummary';
 import prisma from '@/prisma/client';
+import { Flex, Grid } from '@radix-ui/themes';
 import IssueChart from './IssueChart';
+import IssueSummary from './IssueSummary';
+import LatestIssues from './LatestIssues';
 export default async function Home({
   searchParams,
 }: {
@@ -14,5 +13,13 @@ export default async function Home({
     where: { status: 'IN_PROGRESS' },
   });
   const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
-  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
+  return (
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
